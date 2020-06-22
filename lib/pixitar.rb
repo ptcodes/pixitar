a8b13e1203870da776b33ca2aa9177e72f72a796
Pavel Timofeev <pav.timofeev@gmail.com>
2020-06-22 15:59:14 -0400

    update code

--- lib/pixitar.rb
+++ lib/pixitar.rb
@@ -1,6 +1,100 @@
+require "chunky_png"
 require "pixitar/version"
 
 module Pixitar
-  class Error < StandardError; end
-  # Your code goes here...
+  class Image
+    attr_reader :image_lib
+    attr_reader :image
+
+    def initialize(image_lib = ChunkyPNG::Image, height = 400, width = 400)
+      @image_lib = image_lib
+      @image = image_lib.new(height, width)
+    end
+
+    def compose(filename)
+      canvas = image_lib.from_file(filename)
+      image.compose!(canvas, 0, 0)
+    end
+
+    def save(filename)
+      image.save(filename, interlace: true)
+    end
+  end
+
+  class Avatar
+    class InvalidGenderError < StandardError; end
+    class MissingImageFilesError < StandardError; end
+
+    attr_reader :image
+    attr_reader :assets_path
+    attr_reader :image_extention
+    attr_reader :gender
+
+    def initialize(image_klass = Pixitar::Image.new, opts = {})
+      @image = image_klass
+      @assets_path = opts.fetch(:assets_path, "images")
+      @image_extention = opts.fetch(:image_extention, "png")
+    end
+
+    def face_parts
+      [
+        :background,
+        :clothes,
+        :face,
+        :hair,
+        :mouth,
+        :eye
+      ]
+    end
+
+    def genders
+      [
+        :male,
+        :female
+      ]
+    end
+
+    def random_gender
+      genders.sample
+    end
+
+    def male_avatar
+      generate_avatar(:male)
+    end
+
+    def female_avatar
+      generate_avatar(:female)
+    end
+
+    def generate_avatar(gender = random_gender, filename = "avatar.png")
+      @gender = gender
+      raise InvalidGenderError unless genders.include?(gender)
+
+      create_avatar_image(filename)
+    end
+
+    private
+
+    def create_avatar_image(filename)
+      face_parts.map do |face_part|
+        asset = random_asset_for(face_part)
+        image.compose(asset)
+      end
+      image.save(filename)
+    end
+
+    def random_asset_for(face_part)
+      parts = assets.grep(/#{face_part}/)
+      raise MissingImageFilesError, "Missing #{face_part} image files for #{gender} avatars" if parts.empty?
+      parts.sample
+    end
+
+    def assets
+      @assets ||= Dir.glob(path)
+    end
+
+    def path
+      File.join(assets_path, gender.to_s, "*.#{image_extention}")
+    end
+  end
 end
--- pixitar.gemspec
+++ pixitar.gemspec
@@ -6,17 +6,17 @@ Gem::Specification.new do |spec|
   spec.authors       = ["Pavel Timofeev"]
   spec.email         = ["pav.timofeev@gmail.com"]
 
-  spec.summary       = %q{TODO: Write a short summary, because RubyGems requires one.}
-  spec.description   = %q{TODO: Write a longer description or delete this line.}
-  spec.homepage      = "TODO: Put your gem's website or public repo URL here."
+  spec.summary       = "Avatar generation library"
+  spec.description   = "Pixitar generates random pixel avatar"
+  spec.homepage      = "https://github.com/ptcodes/pixitar"
   spec.license       = "MIT"
   spec.required_ruby_version = Gem::Requirement.new(">= 2.3.0")
-
-  spec.metadata["allowed_push_host"] = "TODO: Set to 'http://mygemserver.com'"
+  spec.add_runtime_dependency "chunky_png", "~> 1.3.11"
+  spec.add_development_dependency "minitest", "~> 5.0"
 
   spec.metadata["homepage_uri"] = spec.homepage
-  spec.metadata["source_code_uri"] = "TODO: Put your gem's public repo URL here."
-  spec.metadata["changelog_uri"] = "TODO: Put your gem's CHANGELOG.md URL here."
+  spec.metadata["source_code_uri"] = "https://github.com/ptcodes/pixitar.git"
+  spec.metadata["changelog_uri"] = "https://github.com/ptcodes/pixitar/CHANGELOG.md"
 
   # Specify which files should be added to the gem when it is released.
   # The `git ls-files -z` loads the files in the RubyGem that have been added into git.
