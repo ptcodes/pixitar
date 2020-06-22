51dff619ca219f61bcd9ab0e5fb3bebf877d86e9
Pavel Timofeev <pav.timofeev@gmail.com>
2020-06-22 18:52:47 -0400

    move coveralls to the top

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,7 +1,8 @@
 $LOAD_PATH.unshift File.expand_path("../lib", __dir__)
 require "chunky_png"
-require "pixitar"
 require "minitest/autorun"
 require "coveralls"
 Coveralls.wear!
+
+require "pixitar"
 
